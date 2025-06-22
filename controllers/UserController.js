296684bfc5c64ec223e92c9da0ab8de8873f4d01
fabriziom90 <fabriziomastrobattista@gmail.com>
2025-06-22 12:49:58 +0200

    [UPD] Done login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,8 +1,45 @@
-const login = (req, res) => {
-    const { username, password } = req.body;
-    console.log(username, password)
+const {validationResult} = require('express-validator');
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-    res.send('Login');
+const login = async (req, res) => {
+    try{
+        // validations of fields
+        const validations = validationResult(req);
+        // if result of validation is not empty return an error message for the field
+        if(!validations.isEmpty()){
+            return res.status(200).json({result: false, message:validations.errors[0].msg})
+        }
+
+        // get the form fields
+        const { username, password } = req.body;
+
+        // find the user
+        const user = await User.findOne({username});
+        if(!user) return res.status(401).json({result: false, message: "Utente non trovato"})
+        
+        // check the password sended by the user with the password saved in the schema
+        const checkPassword = await bcrypt.compare(password, user.password);
+        if(!checkPassword) return res.status(401).json({result:false, message: "La password inserita non è sbagliata"});
+
+        // create JWT
+        const token = jwt.sign(
+            {userId: user.id, role: user.role},
+            process.env.SECRET_JWT,
+            {expiresIn: "2h"}
+        )
+
+        res.json({token});
+
+    }
+    catch(err){
+        res.status(500).json({
+            result: false,
+            message: "Qualcosa è andato storto. Riprova più tardi o contatta lo sviluppatore indicandogli l'errore seguente: "+err.message
+        })
+    }
+
 }
 
 module.exports = {
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const { check } = require('express-validator');
 const router = express.Router();
 
 const UserController = require('../controllers/UserController');
 
-router.post('/login', UserController.login);
+router.post('/login', [
+    check('username').notEmpty().trim().withMessage('Devi inserire la username'),
+    check('password').notEmpty().trim().withMessage('Devi inserire la password')
+], UserController.login);
 
 module.exports = router;
