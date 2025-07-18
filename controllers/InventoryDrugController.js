500b9ba5c6876aac65391c6d24b7e105d0edc739
fabriziom90 <fabriziomastrobattista@gmail.com>
2025-07-18 18:11:27 +0200

    [UPD] Done methods for create inventory drug and delete inventory drug

--- controllers/InventoryDrugController.js
+++ controllers/InventoryDrugController.js
@@ -1,3 +1,4 @@
+const { validationResult } = require('express-validator');
 const InventoryDrug = require('../models/InventoryDrug.js');
 
 const index = async (req, res) => {
@@ -12,12 +13,12 @@ const editQuantity = async (req, res) => {
     const inventoryDrug = await InventoryDrug.findByIdAndUpdate(inventoryId, {quantity: quantity})
 
     if(!inventoryDrug){
-        return res.status(404).json({ message: "Prodotto non trovato nell'inventario"})
+        return res.status(404).json({ message: "Medicinale non trovato nell'inventario"})
     }
 
     res.status(200).json({
         result: true,
-        message: "Quantità del prodotto nella stanza aggiornata."
+        message: "Quantità del medicinale nella stanza aggiornata."
     })
 }
 
@@ -28,17 +29,61 @@ const editExpirationDate = async (req, res) => {
     const inventoryDrug = await InventoryDrug.findByIdAndUpdate(inventoryId, {expirationDate: date});
 
     if(!inventoryDrug){
-        return res.status(404).json({ message: "Prodotto non trovato nell'inventario"});
+        return res.status(404).json({ message: "Medicinale non trovato nell'inventario"});
     }
 
     res.status(200).json({
         result: true,
-        message: "Data di scadenza del prodotto nella stanza aggiornata"
+        message: "Data di scadenza del medicinale nella stanza aggiornata"
+    })
+}
+
+const createInventoryDrug = async (req, res) => {
+    try{
+        const validation = validationResult(req)
+        
+        if(!validation.isEmpty()){
+            return res.status(500).json({ result: true, message: validation.errors[0].msg});
+        }
+
+        const { drug, roomId, quantity, expirationDate } = req.body;
+
+        const newInventoryDrug = new InventoryDrug({drug, clinicRoom: roomId, quantity, expirationDate });
+        await newInventoryDrug.save();
+
+        res.status(200).json({
+            result: true,
+            message: "Nuovo inserimento di medicinale completato correttamente"
+        })
+    }
+    catch(err){
+        res.status(500).json({
+            result: false,
+            message: 'Errore durante l\'inserimento. Contattare l\'amministratore del sistema. '+err
+        });
+    }
+    
+}
+
+const deleteInventoryDrug = async (req, res) => {
+    const drugId = req.params.id;
+
+    const deleted = await InventoryDrug.findByIdAndDelete(drugId);
+
+    if(!deleted){
+        return res.status(404).json({ message: 'Medicinale dell\'inventario non trovato'});
+    }
+
+    res.status(200).json({
+        result: true,
+        message: 'Medicinale dell\'inventario cancellato correttamente'
     })
 }
 
 module.exports = {
     index,
     editQuantity,
-    editExpirationDate
+    editExpirationDate,
+    createInventoryDrug,
+    deleteInventoryDrug
 }
--- routes/inventoryDrugRoutes.js
+++ routes/inventoryDrugRoutes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 const authMiddleware = require('../middlewares/authMiddleware');
 const InventoryDrugController = require('../controllers/InventoryDrugController');
 
@@ -7,5 +8,11 @@ const router = express.Router();
 router.get('/:id', authMiddleware, InventoryDrugController.index);
 router.patch('/:id/edit_quantity', authMiddleware, InventoryDrugController.editQuantity);
 router.patch('/:id/edit_expiration_date', authMiddleware, InventoryDrugController.editExpirationDate);
+router.post('/create', [
+    check('drug').notEmpty().withMessage('Devi selezionare prima il medicinale'),
+    check('quantity').notEmpty().trim().withMessage('Devi inserire la quantità').isNumeric().withMessage('Devi inserire un valore numerico'),
+    check('expirationDate').notEmpty().trim().withMessage('Devi inserire la data di scadenza').isISO8601().withMessage('La scadenza deve essere una data valida')
+], authMiddleware, InventoryDrugController.createInventoryDrug);
+router.delete('/:id/delete', authMiddleware, InventoryDrugController.deleteInventoryDrug);
 
 module.exports = router;
