7264521a66fb7c0b246ec757fd88a4316cee48c5
fabriziom90 <fabriziomastrobattista@gmail.com>
2025-09-22 20:14:28 +0200

    [UPD] Updated Task model for calendar view

--- models/Task.js
+++ models/Task.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = mongoose.Schema({
-    text: { type: String, required: true},
+    title: { type: String, required: true},
+    content: { type: String, required: true},
     start: { type: Date, required: true},
     end: { type: Date, required: true},
     done: { type: Boolean, required: true}
