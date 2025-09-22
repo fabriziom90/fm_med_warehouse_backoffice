fa1a8414e023a2029db90d2892511edc1367678f
fabriziom90 <fabriziomastrobattista@gmail.com>
2025-09-22 20:12:28 +0200

    [UPD] Updated task model part 2

--- models/Task.js
+++ models/Task.js
@@ -2,18 +2,8 @@ const mongoose = require('mongoose');
 
 const TaskSchema = mongoose.Schema({
     text: { type: String, required: true},
-    date: { type: Date, required: true},
-    hour: {
-        type: String,
-        required: true,
-        validate: {
-            validator: function(v) {
-                // Controlla formato HH:mm
-                return /^([01]\d|2[0-3]):([0-5]\d)$/.test(v);
-            },
-            message: props => `${props.value} non è un orario valido (HH:mm)!`
-        }
-    },
+    start: { type: Date, required: true},
+    end: { type: Date, required: true},
     done: { type: Boolean, required: true}
 })
 
